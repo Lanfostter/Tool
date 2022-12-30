c56c79c1768c0bdc7873d365074103a81bcd9ca7
DucAnh <fostter2@gmail.com>
2022-12-30 07:24:03 +0700

    id

--- src/main/java/com/example/tool/Listener/ApplicationStartupListener.java
+++ src/main/java/com/example/tool/Listener/ApplicationStartupListener.java
@@ -1,5 +1,7 @@
 package com.example.tool.Listener;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -26,6 +28,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.weaving.DefaultContextLoadTimeWeaver;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -38,6 +41,7 @@ import com.example.tool.edudto.CourseRegisterViewDto;
 import com.example.tool.edudto.CourseSubjectDto;
 import com.example.tool.edudto.StudentCourseRegisterViewDto;
 import com.example.tool.edudto.StudentCourseSubjectDto;
+import com.example.tool.edudto.StudentDto;
 import com.example.tool.edudto.SubjectRegistrationDto;
 import com.example.tool.edudto.TimeTableDto;
 import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
@@ -57,21 +61,32 @@ public class ApplicationStartupListener implements ApplicationListener<ContextRe
 			login.postLogin(dto.getUsername(), dto.getPassword(), "http://" + login.url);
 			System.out.println(login.token_type + " " + login.access_token);
 			RegisterSubject subject = new RegisterSubject();
+			String id = null;
+			try {
+				id = subject.findStudentByStudentCode("1951060525", login.access_token, login.token_type);
+			} catch (NoSuchFieldException | SecurityException | ParseException | IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
 			StudentCourseRegisterViewDto studentCourseRegisterViewDto = subject.testRegisterSubject(login.access_token,
-					login.token_type);
+					login.token_type, id);
 			CourseRegisterViewDto courseRegisterViewDto = studentCourseRegisterViewDto.getCourseRegisterViewObject();
 			for (SubjectRegistrationDto registrationDto : courseRegisterViewDto.getListSubjectRegistrationDtos()) {
 //				StudentCourseSubjectDto courseSubjectDto = subject.addRegister(login.token_type, login.access_token);
 				for (CourseSubjectDto courseSubjectDto : registrationDto.getCourseSubjectDtos()) {
-					for(TimeTableDto timeTableDto : courseSubjectDto.getTimetables()) {
+					for (TimeTableDto timeTableDto : courseSubjectDto.getTimetables()) {
 						courseSubjectDto.setTimetables(new ArrayList<>());
 						courseSubjectDto.getTimetables().add(timeTableDto);
 						ObjectWriter mapper = new ObjectMapper().writer().withDefaultPrettyPrinter();
 						try {
 							String jsonString = mapper.writeValueAsString(courseSubjectDto);
-							System.out.print(courseSubjectDto.getDisplayName());
-							StudentCourseSubjectDto ScsDto = subject.addRegister(login.access_token, login.token_type,
-									jsonString);
+							System.out.println(courseSubjectDto.getDisplayName());
+							StudentCourseSubjectDto scsDto = subject.addRegister(login.access_token, login.token_type,
+									jsonString, id);
+							if (scsDto != null) {
+								break;
+							}
 						} catch (JsonGenerationException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
@@ -82,9 +97,7 @@ public class ApplicationStartupListener implements ApplicationListener<ContextRe
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-						break;
 					}
-					break;
 				}
 
 			}
@@ -98,7 +111,6 @@ public class ApplicationStartupListener implements ApplicationListener<ContextRe
 		List<UserDto> dtos = new ArrayList<>();
 		try {
 			URL url = getClass().getClassLoader().getResource(fileJson);
-			System.out.println(url.getPath());
 			FileReader fileReader = new FileReader(url.getPath());
 			object = jsonParser.parse(fileReader);
 			JSONArray jsonStus = (JSONArray) object;
--- src/main/java/com/example/tool/RegisterSubject.java
+++ src/main/java/com/example/tool/RegisterSubject.java
@@ -1,7 +1,22 @@
 package com.example.tool;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.apache.poi.ss.formula.functions.T;
+import org.bson.json.JsonObject;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -11,41 +26,64 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.tool.dto.RestResponsePage;
 import com.example.tool.edudto.StudentCourseRegisterViewDto;
 import com.example.tool.edudto.StudentCourseSubjectDto;
 import com.example.tool.edudto.StudentDto;
-
-
-
-
 
 public class RegisterSubject {
 	private static String HostPath = "http://localhost:8081/education/api/cs_reg_mongo/findByPeriod/";
 	private static String HostPath1 = "http://localhost:8081/education/api/cs_reg_mongo/add-register/";
 
-	public StudentCourseRegisterViewDto testRegisterSubject(String accessToken, String tokenType) {
+	public StudentCourseRegisterViewDto testRegisterSubject(String accessToken, String tokenType, String stuId) {
 		RestTemplate restTemplate = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", tokenType + " " + accessToken);
 		headers.add("Content-Type", "application/x-www-form-urlencoded");
-		String url = HostPath + "568/18";
+		String url = HostPath +stuId+"/18";
 		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
 		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
 		HttpEntity<T> entity = new HttpEntity<T>((T) null, headers);
-		ResponseEntity<StudentCourseRegisterViewDto> response = restTemplate.exchange(url, HttpMethod.GET, entity,StudentCourseRegisterViewDto.class);
+		ResponseEntity<StudentCourseRegisterViewDto> response = restTemplate.exchange(url, HttpMethod.GET, entity,
+				StudentCourseRegisterViewDto.class);
 		return response.getBody();
 	}
-	public StudentCourseSubjectDto addRegister(String accessToken, String tokenType, String json) {
+
+	public StudentCourseSubjectDto addRegister(String accessToken, String tokenType, String json, String stuId) {
 		RestTemplate restTemplate = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", tokenType + " " + accessToken);
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		headers.add("Content-Type", "application/x-www-form-urlencoded");
-		String url = HostPath1 + "568/18";
-		  HttpEntity<String> request = 
-			      new HttpEntity<String>(json, headers);
-		ResponseEntity<StudentCourseSubjectDto> response = restTemplate.postForEntity(url, request, StudentCourseSubjectDto.class);
-		System.out.println(response.getBody().getStatus());
-		return response.getBody();	
+		String url = HostPath1 +stuId+"/18";
+		HttpEntity<String> request = new HttpEntity<String>(json, headers);
+		ResponseEntity<StudentCourseSubjectDto> response = restTemplate.postForEntity(url, request,
+				StudentCourseSubjectDto.class);
+		return response.getBody();
+	}
+
+	public String findStudentByStudentCode(String studentCode, String accessToken, String tokenType) throws ParseException, JsonGenerationException, JsonMappingException, IOException, NoSuchFieldException, SecurityException {
+		String url = "http://localhost:8081/education/api/student/studentCodeDto/" + studentCode + "/1/100";
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", tokenType + " " + accessToken);
+		headers.add("Content-Type", "application/x-www-form-urlencoded");
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+		HttpEntity<T> entity = new HttpEntity<T>((T) null, headers);
+		ParameterizedTypeReference<RestResponsePage<Object>> responseType = new ParameterizedTypeReference<RestResponsePage<Object>>() {
+		};
+		ResponseEntity<JSONObject> response = restTemplate.exchange(url, HttpMethod.GET, entity, JSONObject.class);
+		ArrayList<Object> arrayList = (ArrayList<Object>) response.getBody().get("content");
+		String id= null;
+		for(Object object : arrayList) {
+			ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+			String json = ow.writeValueAsString(object);
+			JSONParser parser = new JSONParser();
+			JSONObject jsonObject = (JSONObject) parser.parse(json);
+			System.out.println(jsonObject.get("id"));
+			id =  String.valueOf(jsonObject.get("id"));
+		}
+		return id;
 	}
 }
